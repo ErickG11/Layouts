d30e12b88fbcee41c130bb78bc7f5d36ee8f077d
ErickG11 <grandaerick8@gmail.com>
2024-11-14 10:58:16 -0500

    FlexLayout - click en la imagen que redirige a AbsoluteLayour

--- Layouts/FlexLayoutPage.xaml.cs
+++ Layouts/FlexLayoutPage.xaml.cs
@@ -5,5 +5,11 @@ public partial class FlexLayoutPage : ContentPage
 	public FlexLayoutPage()
 	{
 		InitializeComponent();
+
 	}
+    private async void OnImageTapped(object sender, EventArgs e)
+    {
+        // Navega a la página de AbsoluteLayout cuando se hace clic en una imagen
+        await Navigation.PushAsync(new AbsoluteLayoutPage());
+    }
 }
